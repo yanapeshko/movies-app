3a6ca806b0c82eecec5379452a3506dfc23a65cf
Yana Peshko <yanapeshko917@gmail.com>
2023-02-16 21:47:00 -0700

    changed the api

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,30 +1,33 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://movies-app-yanapeshko.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
   paramsSerializer: {
-    encode: params => queryString.stringify(params)
-  }
+    encode: (params) => queryString.stringify(params),
+  },
 });
 
-privateClient.interceptors.request.use(async config => {
+privateClient.interceptors.request.use(async (config) => {
   return {
     ...config,
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${localStorage.getItem("actkn")}`
-    }
+      Authorization: `Bearer ${localStorage.getItem("actkn")}`,
+    },
   };
 });
 
-privateClient.interceptors.response.use((response) => {
-  if (response && response.data) return response.data;
-  return response;
-}, (err) => {
-  throw err.response.data;
-});
+privateClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (err) => {
+    throw err.response.data;
+  }
+);
 
 export default privateClient;
--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,29 +1,32 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://movies-app-yanapeshko.vercel.app/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
   paramsSerializer: {
-    encode: params => queryString.stringify(params)
-  }
+    encode: (params) => queryString.stringify(params),
+  },
 });
 
-publicClient.interceptors.request.use(async config => {
+publicClient.interceptors.request.use(async (config) => {
   return {
     ...config,
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   };
 });
 
-publicClient.interceptors.response.use((response) => {
-  if (response && response.data) return response.data;
-  return response;
-}, (err) => {
-  throw err.response.data;
-});
+publicClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (err) => {
+    throw err.response.data;
+  }
+);
 
 export default publicClient;
