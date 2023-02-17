296408ca3e287926c1e6729cb041454e42ab18a0
Yana Peshko <yanapeshko917@gmail.com>
2023-02-16 21:56:46 -0700

    fixing

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movies-app-yanapeshko.vercel.app/api/v1/";
+const baseURL = "https://movies-app-three-kappa.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
