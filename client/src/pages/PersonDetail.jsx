9ac65fa24323e44e3eb54b65c47ad31e0036fe43
Yana Peshko <yanapeshko917@gmail.com>
2023-02-27 20:29:10 -0700

    fixied grammar mistake

--- client/src/pages/PersonDetail.jsx
+++ client/src/pages/PersonDetail.jsx
@@ -37,7 +37,7 @@ const PersonDetail = () => {
             <Box
               sx={{
                 position: "relative",
-                dispaly: "flex",
+                display: "flex",
                 flexDirection: { xs: "column", md: "row" },
               }}
             >
